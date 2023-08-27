04a72ac76f9a04f036c7da5b71dbb40486ff593f
devinferdynandjobs <jobsdevin2@gmail.com>
2023-08-27 11:40:51 +0700

    ecommarket

--- src/store/modules/product.js
+++ src/store/modules/product.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-
 export default {
     namespaced: true,
     state: {
         products: [],
-        product: []
+        getid: [],
+        cart: [],
     },
     getters: {
         getProduct: state => state.products
@@ -12,30 +12,33 @@ export default {
     actions: {
         async fetchProduct({ commit }) {
             try {
-                const dataProduct = await axios.get("https://ecommerce.olipiskandar.com/api/v1/product/latest/20")
-                commit('SET_PRODUCT', dataProduct.data.data)
-                // console.log(dataProduct.data.data)
+                const dataProduct = await axios.get("https://ecommerce.olipiskandar.com/api/v1/product/search")
+                commit('SET_PRODUCT', dataProduct.data)
             } catch (error) {
                 alert("Ada error");
                 console.log(error);
             }
         },
-        async fetchProductBySlug({ commit }, product_slug) {
+        async fetchProductById({ commit }, slug) {
             try {
-                const dataProduct = await axios.get(`https://ecommerce.olipiskandar.com/api/v1/product/details/${product_slug}`)
-                commit('SET_PRODUCT_BY_SLUG', dataProduct.data.data)
+                const dataProduct = await axios.get(`https://ecommerce.olipiskandar.com/api/v1/product/details/${slug}`)
+                commit('SET_PRODUCT_BY_ID', dataProduct.data)
             } catch (error) {
                 alert("Ada error");
                 console.log(error);
             }
-        }
+        },
+
     },
     mutations: {
         SET_PRODUCT(state, products) {
             state.products = products
         },
-        SET_PRODUCT_BY_SLUG(state, products) {
-            state.product = products
-        }
+        SET_PRODUCT_BY_ID(state, products) {
+            state.getid = products
+        },
+        SET_KERANJANG_GET(state, cart){
+            state.cart = cart;
+        },
     }
 }
--- src/views/ProductBySlugView.vue
+++ src/views/ProductBySlugView.vue
@@ -1,122 +1,156 @@
 <template>
-    <div class="min-h-screen dark:text-white">
-        <div class="flex">
-            <div class="mx-4 xl:max-w-7xl xl:mx-auto">
-                <div class="lg:container sm:grid sm:grid-cols-2 sm:gap-2">
-                    <div class="p-[40px] flex" v-if="product.brand != undefined">
-                        <img class="shadow my-auto" :src="product.brand.logo" :alt="product.name">
-                    </div>
-                    <div class="p-4 flex">
-                        <div class="my-auto">
-                            <h2 class="text-2xl font-semibold">{{ product.name }}</h2>
-                            <p class="text-sm text-gray-500">{{ product.slug }}</p>
-                            <div class="my-10" v-if="product.base_price != undefined">
-                                <b class="text-4xl font-bold font-mono">{{ 'Rp.' + product.base_price }}</b>
-                            </div>
-                            <!-- <div v-if="product.rating != undefined" class="text-xl group my-4">
-                            <span
-                                class="font-bold text-blue-700 bg-blue-300 rounded-lg text-base mx-3 px-2 group-hover:bg-opacity-75">{{
-                                    product.rating.rate }}</span>
-                            <span class="text-yellow-400 hover:scale-105 transition-all group-hover:text-yellow-300"
-                                v-html="rateToStar(product.rating.rate)"></span>
-                            <span
-                                class="font-bold text-blue-700 bg-blue-300 rounded-lg text-base mx-3 px-2 group-hover:bg-opacity-75"><i
-                                    class="bi bi-people"></i> {{ product.rating.count }}</span>
-                            </div> -->
-                            <div class="my-4">
-                                <button @click="decrementQty"
-                                class="text-xl dark:text-white bg-gray-100 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg px-4 py-2 text-center mr-1 md:mr-1 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-500 transition-all">
-                                    &minus;
-                                </button>
-                                <input class="text-xl dark:text-white bg-gray-50 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg px-4 py-2 text-center mr-1 md:mr-1 dark:bg-gray-700 dark:hover:bg-gray-600 w-24 dark:focus:ring-gray-500 transition-all"
-                                type="number" min="1" v-model="qty" name="qty" id="qty">
-                                <button @click="incrementQty"
-                                class="text-xl dark:text-white bg-gray-100 hover:bg-gray-400 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg px-4 py-2 text-center mr-1 md:mr-1 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-500 transition-all">
-                                    &plus;
-                                </button>
-                                <!-- {{ qty }} -->
-                            </div>
-                            <div>
-                                <!-- <p>{{ product.description }}</p> -->
-                                <button v-if="isAuthenticated" @click="addCartData({variation_id: product.variations[0].id, qty })"
-                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-1 md:mr-1 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                    Tambah ke keranjang
-                                </button>
-                                <button v-else @click="$router.push('/login')"
-                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-1 md:mr-1 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                    Tambah ke keranjang
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- {{ product }} -->
-        </div>
-        <!-- {{ product.variations.id }} -->
-        <!-- {{ product.variations }} -->
-    </div>
+ 
+  <div v-if="getid.data == undefined">
+   <center>
+     <br /><br /><br /><br />
+     <div role="status">
+       <svg
+         aria-hidden="true"
+         class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+         viewBox="0 0 100 101"
+         fill="none"
+         xmlns="http://www.w3.org/2000/svg"
+       >
+         <path
+           d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+           fill="currentColor"
+         />
+         <path
+           d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+           fill="currentFill"
+         />
+       </svg>
+       <span class="sr-only">Loading...</span>
+     </div>
+   </center>
+  </div>
+  <div v-else>
+     <section class="text-gray-700 body-font overflow-hidden bg-white">
+         <div class="container px-5 py-24 mx-auto">
+           <div class="lg:w-4/5 mx-auto flex flex-wrap">
+             <img alt="ecommerce" class="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src="https://www.whitmorerarebooks.com/pictures/medium/2465.jpg">
+             <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+               <h2 class="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
+               <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{getid.data.name}}</h1>
+               <div class="flex mb-4">
+                 <span class="flex items-center">
+                   <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
+                     <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                   </svg>
+                   <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
+                     <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                   </svg>
+                   <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
+                     <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                   </svg>
+                   <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
+                     <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                   </svg>
+                   <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-red-500" viewBox="0 0 24 24">
+                     <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                   </svg>
+                   <span class="text-gray-600 ml-3">4 Reviews</span>
+                 </span>
+                 <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
+                   <a class="text-gray-500">
+                     <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                       <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                     </svg>
+                   </a>
+                   <a class="ml-2 text-gray-500">
+                     <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                       <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                     </svg>
+                   </a>
+                   <a class="ml-2 text-gray-500">
+                     <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                       <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                     </svg>
+                   </a>
+                 </span>
+               </div>
+               <p class="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
+               <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
+                 <div class="flex">
+                   <span class="mr-3">Color</span>
+                   <button class="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
+                   <button class="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                   <button class="border-2 border-gray-300 ml-1 bg-red-500 rounded-full w-6 h-6 focus:outline-none"></button>
+                 </div>
+                 <div class="flex ml-6 items-center">
+                   <span class="mr-3">Size</span>
+                   <div class="relative">
+                     <select class="rounded border appearance-none border-gray-400 py-2 focus:outline-none focus:border-red-500 text-base pl-3 pr-10">
+                       <option>SM</option>
+                       <option>M</option>
+                       <option>L</option>
+                       <option>XL</option>
+                     </select>
+                     <span class="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+                       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4" viewBox="0 0 24 24">
+                         <path d="M6 9l6 6 6-6"></path>
+                       </svg>
+                     </span>
+                   </div>
+                 </div>
+               </div>
+               <div class="flex">
+                 <span class="title-font font-medium text-2xl text-gray-900">$58.00</span>
+                 <div class="flex items-center">
+                   &nbsp;
+                   <button v-on:click="kurang" class="border rounded-md py-2 px-4 mr-2">-</button>
+                    <input type="text" v-model="counter" hidden>
+                    <span>{{counter}}</span>
+                   <button v-on:click="tambah" class="border rounded-md py-2 px-4 ml-2">+</button>
+                 </div> &nbsp;
+                 <button v-if="token"  @click="addtocart({variation_id : getid.data.variations[0].id,qty:counter})" on class="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Keranjang</button>
+                 <button v-else  v-on:click="nontifikasi" class="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">Keranjang</button>
+               </div>
+               <br><br><br>
+             
+             </div>
+           </div>
+         </div>
+       </section>
+  </div>
 </template>
-
 <script>
-import { mapGetters, mapState, mapActions } from "vuex";
-import { onMounted } from "vue";
-// import product from "@/store/modules/product";
-
+import { mapActions, mapGetters, mapState } from 'vuex'
 export default {
-    props: [
-        'product_slug'
-    ],
-    data() {
-        return {
-            // product: null,
-            // rateToStar(val) {
-            //     const starFill = '<i class="bi bi-star-fill"></i>';
-            //     const starHalf = '<i class="bi bi-star-half"></i>';
-            //     const star = '<i class="bi bi-star"></i>';
-            //     let txtHTML = '';
-            //     for (let i = 0; i <= 4; i++) {
-            //         if (val - i >= 0.75) {
-            //             txtHTML += starFill;
-            //         }
-            //         else if (val - i > 0.25) {
-            //             txtHTML += starHalf;
-            //         }
-            //         else {
-            //             txtHTML += star;
-            //         }
-            //     }
-            //     return txtHTML;
-            // },
-            qty: 1
-        }
-    },
-    methods: {
-        decrementQty(){
-            if (this.qty > 1) {
-                this.qty--;
-            }
-        },
-        incrementQty(){
-            this.qty++;
-        },
-        ...mapActions('cart', ['addCartData'])
-    },
-    setup(props, context) {
-        onMounted(() => {
-            try {
-                context.emit("id-menu", 0)
-            } catch (error) {
-                console.log(error)
-            }
-        })
-    },
-    mounted() {
-        this.$store.dispatch("product/fetchProductBySlug", this.product_slug)
-    },
-    computed: {
-        ...mapState('product', ['product']),
-        ...mapGetters('auth', ['isAuthenticated'])
-    }
+ data(){
+   return{
+     counter : 1
+     
+   }
+ },
+ props:[
+     'slug'
+ ],
+ mounted() {
+     this.$store.dispatch("product/fetchProductById", this.slug)
+ },
+ computed: {
+     ...mapState('product', ['getid']),
+     ...mapGetters('auth', ['isAuthenticated']),
+     ...mapState('auth',['token']),
+ },
+ methods:{
+   ...mapActions('cart',['addtocart']),
+   nontifikasi(){
+     alert('login hela bro');
+     this.$router.push('/login/' + this.slug);
+   },
+   tambah(){
+         this.counter++
+     },
+   kurang(){
+       if(this.counter > 1){
+             this.counter--;
+       }
+     }
+ }
 }
 </script>
+
+
+
